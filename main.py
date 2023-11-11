402b2bd3708c01c7b421ac4ce3c3e9d31a0c0629
Petrus Holm <petrus@synergi.so>
2023-11-11 14:44:42 +0200

    map ambient light over time in vid1

--- main.py
+++ main.py
@@ -5,45 +5,31 @@ from datetime import timedelta
 
 
 ticktime = []
-labels = []
+ambient_light_values = []
 
 for filename in glob.glob('./secrets/Indoor/Participant_1/AFE_*.json'):
     with open(filename, 'r') as file:
         # Parse the JSON data
         data = json.load(file)
 
-        for entry in data:
-            # Check if 'labels' key exists in the entry
-            if 'labels' in entry:
-                # Convert ticktime to mm:ss format
-                # Convert milliseconds to seconds
-                tick_time_seconds = entry['auxSensors']['tempEt']['i'][0] / 1000
-                formatted_time = str(timedelta(seconds=tick_time_seconds))[
-                    2:7]  # Format as mm:ss
-                ticktime.append(formatted_time)
-                # Using 'tempEt' ticktime as an example
-                labels.extend(entry['labels'])
+    for entry in data:
+        # Extracting 'ticktime' and 'Ambient Light' value
+        # Convert milliseconds to seconds
+        tick_time_seconds = entry['auxSensors']['lightAmbient']['i'][0] / 1000
+        formatted_time = str(timedelta(seconds=tick_time_seconds))[
+            2:7]  # Format as mm:ss
+        # Ambient Light value
+        ambient_light_value = entry['auxSensors']['lightAmbient']['v'][2]
 
-# To plot, we need to map non-numeric labels to numbers
-unique_labels = list(set(labels))
-label_mapping = {label: i for i, label in enumerate(unique_labels)}
+        ticktime.append(formatted_time)
+        ambient_light_values.append(ambient_light_value)
 
-# Map the labels to numbers
-mapped_labels = [label_mapping[label] for label in labels]
-
-# Create a scatter plot
-plt.figure(figsize=(15, 6))  # Increase figure size
-plt.scatter(ticktime, mapped_labels)
-
-
-# Display fewer x-axis labels
-# plt.xticks(ticks=plt.xticks()[0], labels=ticktime[::len(ticktime)//10])
-
-
-plt.yticks(range(len(unique_labels)), unique_labels)
+# Create a plot
+plt.figure(figsize=(10, 6))
+plt.plot(ticktime, ambient_light_values, marker='o')
 plt.xlabel('Ticktime (mm:ss)')
-plt.ylabel('Labels')
-plt.title('Labels over Ticktime')
-plt.xticks(rotation=45)  # Rotate x-axis labels for better visibility
+plt.ylabel('Ambient Light Value')
+plt.title('Ambient Light Values over Time')
+plt.xticks(rotation=45)
 plt.grid(True)
 plt.show()
